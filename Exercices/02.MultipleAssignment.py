149e1e725efac1ce8ba3b05cd5467fe1d374e4ab
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-23 10:32:56 -0300

    Multiple assignment finished

---
+++ Exercices/02.MultipleAssignment.py
@@ -0,0 +1,7 @@
+# Multiple assignment
+
+name, age, height = 'Arthur', 32, 1.82
+print(name, age, height)
+
+Spongebob = Patrick = Sandy = Squidward = 30
+print(Spongebob, Patrick, Sandy, Squidward)
