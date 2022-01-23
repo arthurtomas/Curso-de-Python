f863903e2d92986c8e1dd48298caf88b792ba0f1
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-23 12:15:28 -0300

    Type Cast finished

---
+++ Exercices/04.TypeCast.py
@@ -0,0 +1,11 @@
+# Type casting is the ability to convert the data type of a value to another data type
+
+x = 1  # int
+y = 2.0  # float
+z = '3'  # str
+
+print(float(x))
+print(x)
+print(int(y))
+print(z)
+print(int(z)*3)
