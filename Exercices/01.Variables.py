ee4737833336cae3e1eda9a2399472d61a5db8a7
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-23 08:49:13 -0300

    16min37s

---
+++ Exercices/01.Variables.py
@@ -0,0 +1,24 @@
+# Variables
+
+# Strings
+first_name = 'Arthur'
+last_name = 'Barzilay'
+full_name = first_name + ' ' + last_name
+print('Hello ' + full_name)
+print(type(full_name))
+
+# int
+age = 32
+age += 1
+print(f'Your age is {age}')
+print(type(age))
+
+# float
+height = 182.5
+print(f'Your height is {height}cm')
+print(type(height))
+
+# boolean
+human = True
+print(human)
+print(type(human))
