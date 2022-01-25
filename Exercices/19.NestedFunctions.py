85a833a520b7dad03ec8a85c30238c794b947222
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-25 16:18:36 -0300

    Nested functions finished

---
+++ Exercices/19.NestedFunctions.py
@@ -0,0 +1,8 @@
+# num = input("Enter an integer positive number: ")
+# num = float(num)
+# num = abs(num)
+# num = round(num)
+# print(num)
+# This is a way to do i, but bellow you'll see a better way
+
+print(round(abs(float(input("Enter an integer positive number: ")))))
