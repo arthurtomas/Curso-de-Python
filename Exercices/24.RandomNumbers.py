1554d1afe26f96f2d2a1c23cadbe6bbc093b8fa1
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-25 19:12:36 -0300

    Random Numbers updated

--- Exercices/24.RandomNumbers.py
+++ Exercices/24.RandomNumbers.py
@@ -2,16 +2,22 @@ import random
 import time
 
 random_numbers = []
+count_numbers = []
 
-for i in range(100000):
+
+for i in range(1000000):
 
     x = random.randint(1, 6)
     random_numbers.append(x)
-    print(x)
 
 print(random_numbers)
 
 for i in range(1, 7):
     print(f"{i}: {random_numbers.count(i)} occurrences")
+    count_numbers.append(random_numbers.count(i))
 
+num_max = count_numbers.index(max(count_numbers))+1
+counter_max = max(count_numbers)
+
+print(f"The number with the most occurrences is the number {num_max} appearing {counter_max} times.")
 
