2f98b9b9d34e5fe892f7b23209404057c9ecbe50
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-26 11:15:05 -0300

    File detection finished

---
+++ Exercices/26.FileDetection.py
@@ -0,0 +1,19 @@
+import os
+
+path = "C:\\Users\\Barzilay\\Desktop\\folder"
+
+if os.path.exists(path):
+    print("That location exists!")
+    if os.path.isfile(path):
+        print("That is a file!")
+    elif os.path.isdir(path):
+        print("That is a directory!")
+else:
+    print("That location doesn't exists!")
+
+
+
+
+
+
+
