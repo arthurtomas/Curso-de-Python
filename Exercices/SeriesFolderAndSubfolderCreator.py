7cb34721e009410f1d3244b1cc7736a925832030
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-26 19:44:23 -0300

    SFASFC has been improved

--- Exercices/SeriesFolderAndSubfolderCreator.py
+++ Exercices/SeriesFolderAndSubfolderCreator.py
@@ -1,9 +1,11 @@
 import shutil
+import getpass
 import os
 
 # Inputs
 m_folder = input("Series name: ")
 num_sub_folders = int(input("Number of Seasons: "))
+user = getpass.getuser()
 
 
 def delete_folder(directory):
@@ -18,19 +20,19 @@ def create_folder(directory):              # Function responsible to create fold
     os.mkdir(directory)
 
 
-if dir_exists(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}"):
+if dir_exists(f"C:\\Users\\{user}\\Desktop\\{m_folder}"):
     answer = input("This folder already exists. Would you like to delete it and create a new one? (Y/N): ")
     if answer in "Nn":
         print("Ok, we kept your original folder and its content.")
     elif answer in "Yy":
-        delete_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}")
-        create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}")
+        delete_folder(f"C:\\Users\\{user}\\Desktop\\{m_folder}")
+        create_folder(f"C:\\Users\\{user}\\Desktop\\{m_folder}")
         for i in range(1, num_sub_folders + 1):
-            create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}\\{i}° Season")
+            create_folder(f"C:\\Users\\{user}\\Desktop\\{m_folder}\\{i}° Season")
     else:
         print("You didn't enter a valid answer!")
-elif not dir_exists(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}"):
-    create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}")
+elif not dir_exists(f"C:\\Users\\{user}\\Desktop\\{m_folder}"):
+    create_folder(f"C:\\Users\\{user}\\Desktop\\{m_folder}")
     for i in range(1, num_sub_folders + 1):
-        create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}\\{i}° Season")
+        create_folder(f"C:\\Users\\{user}\\Desktop\\{m_folder}\\{i}° Season")
 
