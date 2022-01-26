df757607de38bf3cc7b8eecb000e0dfdd46eeb6f
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-26 12:51:37 -0300

    Write a file finished

---
+++ Exercices/28. WriteAFile.py
@@ -0,0 +1,10 @@
+text = "Have a nice day!\nSee ya!"
+
+# with open("test1.txt", "a") as file:                   # append some text
+#     file.write(text)
+
+# with open("test1.txt", "w") as file:                   # write a text overwriting
+#     file.write(text)
+
+# with open("test1.txt", "r") as file:                   # read a text
+#     file.write(text)
