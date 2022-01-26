4d8882f2e22aac992f185c4cbf4d0df7d008c9b8
Arthur Tomas Da Silva <arthurtomas.ep@gmail.com>
2022-01-26 18:31:00 -0300

    Series Folder And Internal Folder Creator have been fineshed

--- Exercices/SeriesFolderAndSubfolderCreator.py
+++ Exercices/SeriesFolderAndSubfolderCreator.py
@@ -1,3 +1,4 @@
+import shutil
 import os
 
 # Inputs
@@ -6,21 +7,30 @@ num_sub_folders = int(input("Number of Seasons: "))
 
 
 def delete_folder(directory):
-    print("")
+    shutil.rmtree(directory)
+
+
+def dir_exists(directory):
+    return os.path.exists(directory)
 
 
 def create_folder(directory):              # Function responsible to create folders
-    try:
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-    except OSError:
-        print("Error")
+    os.mkdir(directory)
 
 
-# Create the main folder
-create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}")
-
-# Loop for creating the seasons folders
-for i in range(1, num_sub_folders+1):
-    create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}\\{i}° Season")
+if dir_exists(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}"):
+    answer = input("This folder already exists. Would you like to delete it and create a new one? (Y/N): ")
+    if answer in "Nn":
+        print("Ok, we kept your original folder and its content.")
+    elif answer in "Yy":
+        delete_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}")
+        create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}")
+        for i in range(1, num_sub_folders + 1):
+            create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}\\{i}° Season")
+    else:
+        print("You didn't enter a valid answer!")
+elif not dir_exists(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}"):
+    create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}")
+    for i in range(1, num_sub_folders + 1):
+        create_folder(f"C:\\Users\\Barzilay\\Desktop\\{m_folder}\\{i}° Season")
 
